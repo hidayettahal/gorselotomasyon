750653976c53a867291d59102ac49017604d3a5e
hidayettahal <tahal.hidayet79097@gmail.com>
2022-12-14 18:42:47 +0300

    Create formkurs.cs

---
+++ formkurs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace _202503024__Mercan_Esen_
+{
+    public partial class formkurs : Form
+    {
+        public formkurs()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            formbasvurular fr2 = new formbasvurular();
+            fr2.ShowDialog();
+
+        }
+
+    }
+}
